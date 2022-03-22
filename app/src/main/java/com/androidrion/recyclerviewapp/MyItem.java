5c2f3fc126b0bff6fdbe5a3702656da141e43b97
CarloAgastya <carloagastya.ca@gmail.com>
2022-03-22 13:13:37 +0800

    Rycycle View Destinasi Wisata Dunia

---
+++ app/src/main/java/com/androidrion/recyclerviewapp/MyItem.java
@@ -0,0 +1,21 @@
+package com.androidrion.recyclerviewapp;
+
+class MyItem {
+
+    static int[] iconList = {
+            R.drawable.pulaumaladewa,
+            R.drawable.bendhorse, R.drawable. cappadocia,
+            R.drawable.danaumoraine, R.drawable.kotapetra, R.drawable.rajaampat,
+            R.drawable.seljalan, R.drawable.tamanhitachi,
+            R.drawable.toresdelpaine, R.drawable.zhangjia
+    };
+
+    static String[] Headline = { "PulauMaladewa", "BendHorseShoe", "Cappadocia", "DanauMoraine",
+            "KotaPetra", "RajaAmpat", "Seljalandsfoss", "TamanHitachi", "TorresDelPaine", "Zhangjiajie"
+    };
+
+    static String[] Subhead = { "Pulau Wisata", "Taman Nasional", "Negeri Balon Udara",
+            "Danau Wisata", "Kota Wisata", "Pulau Wisata", "Air Terjun", "Taman Wisata", "Pemandangan Alam",
+            "Tebing Langit"
+    };
+}
